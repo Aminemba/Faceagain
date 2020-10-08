e9853b14fe9d83192528ee9ba044e6935a3c71d3
Amine <amin.mba@iuj.ac.jp>
2020-10-08 10:15:01 +0000

    The routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "users#new"
+
+  resources :sessions,only: [:new , :create, :destroy]
+  get "/sessions/:id", to: "sessions#destroy"
+
+  resources :users
+  resources :posts ,only: [:new ,:show , :update , :confirm, :edit, :index, :create, :destroy] do
+  get "/post/:id", to: "post#destroy"
+    collection do
+      post :confirm
+    end
+end
+
 end
